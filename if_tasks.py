9bbfed30993c147f992a5c6be604b1b856cc8f6d
unknown <Administrator@E20040-2.mcsd.com.eg>
2024-05-27 21:22:42 +0300

    added if_tasks

---
+++ if_tasks.py
@@ -0,0 +1,98 @@
+# #first question
+# #units =  int(input('insert the number of unit:'))
+# #if units < 100 :
+# #  print('no charge')
+# #elif units >100 and units<200 :
+# #  print('your price is :', units*5) 
+# #elif units >200 :
+# #  print('your price is :', units*10)
+
+
+
+# #second question
+# # marks = float(input("insert your marks"))
+# # if marks >90 :
+# #     print('your grade is A')
+# # elif marks >80 and marks <= 90:
+# #     print('your grade is B')
+# # elif marks >= 60 and marks <= 80 :
+# #     print('your grade is C')
+# # elif marks<60 :
+# #     print('your grade is D')
+# # else :
+# #     print('invalid input')                
+
+
+
+
+
+# # #third question
+# # cost = int(input('insert the price of the bike:'))
+# # if cost >100000 :
+# #     print('your taxes is ', cost *0.15) 
+# # elif cost<=100000 and cost > 50000 :
+# #     print('your taxes is:', cost*0.10)
+# # elif cost <=50000 :
+# #     print('your taxes is', cost *0.05)        
+
+
+
+
+# # fourth question
+# # x = int(input('insert number1:'))
+# # y = int(input('insert number2:'))
+# # print(y,"is larger than",x)if y>x else print(x,"is larger than",y)
+
+
+
+# #fifth question
+# # x=int(input('insert a number:'))
+# # print(x,"is a positive number")if x>0 else print(x,"is an negative number")
+
+
+
+
+# #sixth question
+# # x=int(input("insert your nuber"))
+# # print('x is odd number')if (x%2>0) else print('x is even')
+
+
+
+
+# #seventh question
+# # x= int(input("enter number1:"))
+# # y= int(input("enter number2:"))
+# # z= int(input("enter number3:"))
+# # if x>y and x>z :
+# #     print('x is the largest number')
+# # elif y>x and y>z :
+# #     print('y is the largest number')
+# # elif z>x and z>y :
+# #     print('z is the largest number')        
+
+
+
+
+# #eighth question
+# # working_days= int(input('enter your working days:'))
+# # absent_days= int(input('enter your absent days:'))
+# # atendance_persentage= 1-(absent_days/working_days)
+# # print(atendance_persentage)
+# # print("sorry,you cant sit in exam")if atendance_persentage<0.75 else print("good news , you are accepted in exam")
+
+
+
+# #ninth question
+# percentage= float(input('enter your percentage:'))
+# if percentage<25 :
+#     print('your grade is D')
+# elif percentage>=25 and percentage<45 :
+#     print('your grade is C')
+# elif percentage >= 45 and percentage<50 :
+#     print('your grade is B')
+# elif percentage>= 50 and percentage < 60 :
+#     print('your grade is B+')
+# elif percentage>= 60 and percentage< 80:
+#     print('your grade is A')
+# elif percentage>80 :
+#     print('your grade is A+')    
